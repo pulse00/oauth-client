2a6aca76f420098c6e93cd467c973eae1747ccae
Robert Gruendler <r.gruendler@gmail.com>
2012-10-31 20:52:51 +0100

    'fixed' demo client

--- src/app.php
+++ src/app.php
@@ -1,51 +1,58 @@
 <?php
-
 $app = require __DIR__.'/bootstrap.php';
 
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+
+
+/**
+ * Render the link to the oauth2 authorization page
+ */
 $app->get('/', function() use ($app){
-
-    if (null === $token = $app['session']->get('token')) {
-        return $app->redirect('/authorize');
-    }
-
-    return $app['twig']->render('index.html.twig', array('token' => $app['auth_token']));
+    $url = sprintf($app['authorize_url'], $app['auth_token'], $app['redirect_url']);
+    return $app['twig']->render('index.html.twig', array('token' => $app['session']->get('token'), 'url' => $url));
 });
 
 
-$app->get('/authorize', function() use ($app) {
-
-    $url = 'http://playfm3.local/app_dev.php/oauth/v2/auth?client_id=' . $app['auth_token'] . '&redirect_uri=' . $app['redirect_url'] . '&response_type=code';
-
-    return $app['twig']->render('authorize.html.twig', array(
-            'url' => $url
-    ));
-});
-
+/**
+ * The redirect_uri. Use the OAuth object to get an access token.
+ */
 $app->get('/back', function() use ($app) {
 
     $code = $app['request']->get('code');
 
-//     http://acme.localhost/app_dev.php/oauth/v2/token?client_id=4f8e5bb57f8b9a0816000000_1xwgejzp1e3o8sgosc884cgoko44wgg4gc0s84ckw0c0sk4c4s&client_secret=147v1qcgxvuscg4owg4480ww484kc0ow0cwgkw0c4g4g8oowkc&grant_type=authorization_code&redirect_uri=http%3A%2F%2Fwww.google.com&code=6c7136745d8556650cb5e0d5cd53029c925aae72
-
-
     try {
-        $oauth = new \OAuth($app['auth_token'], $app['auth_secret'], OAUTH_SIG_METHOD_HMACSHA1,OAUTH_AUTH_TYPE_URI);
-        $oauth->enableDebug();
-        $oauth->setToken($code, $app['auth_secret']);
-        $access_token_info = $oauth->getAccessToken($app['token_url']);
+        $oauth = $app['oauth'];
+        $redirect = $app['redirect_url'];
+        $url = $app['token_url'] . sprintf('?grant_type=authorization_code&client_secret=%s&client_id=%s&code=%s&redirect_uri=%s', $app['auth_secret'], $app['auth_token'], $code, urlencode($redirect));
+        $access_token_info = $oauth->getAccessToken($url);
     } catch (\OAuthException $e) {
-        var_dump($e->lastResponse);
+        print_r(json_decode($e->lastResponse));
+        die();
     }
 
-    die();
-    return $app['twig']->render('authorize.html.twig', array(
-            'url' => $url
-    ));
-
+    // the response doesn't contain the proper array from the OAuth documentation,  the first
+    // key holds the raw json response
+    $result = json_decode(array_keys($access_token_info)[0], true);
+    
+    if (!isset($result['access_token'])) {
+        throw new \RuntimeException('Error retrieving access token');
+    }
+    
+    // access token retrieved, redirect back to home
+    $app['session']->set('token', $result['access_token']);
+    return new RedirectResponse('/');
 });
 
-$app->get('/success', function() use ($app) {
 
+$app->get('/request', function() use ($app) {
+    
+    $oauth = $app['oauth'];
+    $oauth->fetch($app['api_url'] . '?access_token=' . $app['session']->get('token'));
+    $response = new Response($oauth->getLastResponse());
+    $response->headers->set('Content-Type', 'application/json');
+     
+    return $response;
 });
 
 return $app;
--- src/bootstrap.php
+++ src/bootstrap.php
@@ -3,8 +3,6 @@
 require_once __DIR__.'/../vendor/autoload.php';
 
 use Silex\Provider\TwigServiceProvider;
-use Silex\Provider\DoctrineServiceProvider;
-use Repository\DumbRepository;
 
 $env = getenv('APPLICATION_ENV') ? getenv('APPLICATION_ENV') : 'production';
 $ini_config = parse_ini_file(__DIR__.'/config.ini', TRUE);
@@ -13,18 +11,24 @@ $config = $ini_config[$env];
 $app = new Silex\Application();
 
 $app['debug'] = true;
-
-$app['auth_token'] = $config['auth_token'];
-$app['auth_secret'] = $config['auth_secret'];
-$app['redirect_url'] = $config['redirect_url'];
-$app['token_url'] = $config['token_url'];
+$app['authorize_url']    = $config['authorize_url'];
+$app['redirect_url']     = $config['redirect_url'];
+$app['token_url']        = $config['token_url'];
+$app['auth_secret']      = $config['auth_secret'];
+$app['auth_token']       = $config['auth_token'];
+$app['api_url']          = $config['api_url'];
 
 $app->register(new TwigServiceProvider(), array(
     'twig.path' => __DIR__.'/templates',
     'twig.options' => array('cache' => __DIR__.'/../cache'),
 ));
 
-$app->register(new Silex\Provider\SessionServiceProvider());
+$app['oauth'] = $app->share(function() use ($app) {
+    $oauth = new \OAuth($app['auth_token'], $app['auth_secret'], OAUTH_SIG_METHOD_HMACSHA1,OAUTH_AUTH_TYPE_URI);
+    $oauth->enableDebug();
+    return $oauth;
+});
 
+$app->register(new Silex\Provider\SessionServiceProvider());
 
 return $app;
